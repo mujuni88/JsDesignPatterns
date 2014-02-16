fe6db73707042891a119aa56ec75ab43c9908a04
Joe Buza <joebm08@gmail.com>
2014-02-15 23:25:45 -0600

    Add .gitignore

--- test/singletonSpec.js
+++ test/singletonSpec.js
@@ -2,8 +2,24 @@
  * Created by joebm08 on 2/6/14.
  */
 
-describe("Testing singleton patten", function(){
-   it("should test for properties",function(){
-       expect(st.getInstance().message).toBe("singleton");
+describe("Testing singleton pattern", function(){
+   var values = {
+       msg:"hello",
+       title:"singleton"
+
+   };
+
+   it("should not return null values",function(){
+       expect(st.getInstance().message()).not.toBeNull();
+       expect(st.getInstance().title()).not.toBeNull();
+   });
+    it("should not return an empty values",function(){
+       expect(st.getInstance().message()).toBeTruthy();
+       expect(st.getInstance().title()).toBeTruthy();
+   });
+
+    it("should return the correct values",function(){
+       expect(st.getInstance().message()).toEqual(values.msg);
+       expect(st.getInstance().title()).toEqual(values.title);
    });
 });
